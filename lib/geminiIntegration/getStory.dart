11793817caae713c4a56e5b50e22239d20e45b7b
Vageesh-Jayaraman <143870355+Vageesh-Jayaraman@users.noreply.github.com>
2024-06-18 14:38:46 +0530

    Update getStory.dart

--- lib/geminiIntegration/getStory.dart
+++ lib/geminiIntegration/getStory.dart
@@ -3,7 +3,7 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 
 
 Future<String> generateStory({required String character}) async {
-  final apiKey = 'AIzaSyAeNYD0nvXlBlqhX8PYLuQ16pVHFKq87B4';
+  final apiKey = API_KEY;
   final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey);
   final prompt = 'Create a new short, cute and funny story for kids with many emojis on this Pokemon character: $character.'
                   'No need of Bold/italics/other text styles. Leave lines wherever required for easy reading. The title should be short '
